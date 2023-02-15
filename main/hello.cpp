e9d1054bada6531b0bb03579bb0aa80145bbf6d9
kevinjacob02 <79471971+kevinjacob02@users.noreply.github.com>
2023-02-15 23:20:20 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "I have successfully made a change in hello.cpp file\n";
+  
   return 0;
 }
